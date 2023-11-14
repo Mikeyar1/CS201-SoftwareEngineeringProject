63192a669534cb16d070c21697db140e8935ed21
braydenm02 <99356492+braydenm02@users.noreply.github.com>
2023-11-14 16:34:17 -0500

    Update event.php

--- event.php
+++ event.php
@@ -17,9 +17,11 @@
         }
     }
     //TODO : days must be an array value
-    $days = $_POST['startDate']
+    $days = $_POST['startDate'];
 
     //TODO : start and end times must be arrays
+    $start_times = $_POST[];
+    $end_times = $_POST[];
 
     //TODO : for all days, create an event with the construct function
     //Name, description are the same for all, unique day and/or times for all
