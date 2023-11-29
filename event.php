c47b7daa8c616e07da8ba24001a27d9119b8df31
braydenm02 <99356492+braydenm02@users.noreply.github.com>
2023-11-29 08:49:44 -0500

    Update event.php

--- event.php
+++ event.php
@@ -1,27 +1,12 @@
 <?php
-    $myfile = fopen("db.csv","");
-    //Event class
-    class Event {
-        public $day;
-        public $name:
-        public $start_time;
-        public $end_time;
-        public $desc;
-
-        public function __construct($name, $day, $start_time, $end_time, $desc) {
-            $this->name = $name;
-            $this->day = $day;
-            $this->start_time = $start_time;
-            $this->end_time = $end_time;
-            $this->desc = $desc;
-        }
-    }
+    $myfile = fopen("db.csv","w");
+    
     //TODO : days must be an array value
     $days = $_POST['startDate'];
 
     //TODO : start and end times must be arrays
-    $start_times = $_POST[];
-    $end_times = $_POST[];
+    $start_times = $_POST[''];
+    $end_times = $_POST[''];
 
     //TODO : for all days, create an event with the construct function
     //Name, description are the same for all, unique day and/or times for all
