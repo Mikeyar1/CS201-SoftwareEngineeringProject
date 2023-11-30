905ff52a79253f8241d18a2ac1bce68f063d7b3b
braydenm02 <99356492+braydenm02@users.noreply.github.com>
2023-11-30 17:59:28 -0500

    Update calendarWeek.js

--- js/calendarWeek.js
+++ js/calendarWeek.js
@@ -1,1 +1,17 @@
 // load everything from database
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "",
+  user: "",
+  password: "",
+  database: ""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
