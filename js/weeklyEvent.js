fad05366c3789d5341186ffcdb5b446b14e0ca2c
dageeem <149324186+dageeem@users.noreply.github.com>
2023-11-30 18:57:00 -0500

    Update addEvent.js

--- js/weeklyEvent.js
+++ js/weeklyEvent.js
@@ -1,17 +1,6 @@
 // adds an event to the database
 function addEvent(name, description, weekday, startTime, endTime) {
 
-  var mysql = require('mysql');
-
-  con.connect(function(err) {
-    if (err) throw err;
+  
     let sqlstr = "INSERT INTO Calendar(name, desc, weekday, startTime, endTime) VALUES ('name', 'description', 'weekday', 'startTime', 'endTime')";
     db.run(sqlstr); // Run the query without returning anything
- const result = stmt.getAsObject({':aval' : 1, ':bval' : 'world'});
-console.log(result); // Will print {a:1, b:'world'}
-, function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-    });
-  });
-};
