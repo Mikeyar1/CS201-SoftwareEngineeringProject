04f0633816be29e2c8f8ff7bc3146c59d818ccf0
dageeem <149324186+dageeem@users.noreply.github.com>
2023-11-30 17:57:17 -0500

    Update weeklyEvent.js

--- js/weeklyEvent.js
+++ js/weeklyEvent.js
@@ -1,30 +1,18 @@
-<!DOCTYPE html>
-<html>
-<body>
+// adds an event to the database
+function addEvent(name, description, weekday, startTime, endTime) {
+var mysql = require('mysql');
 
-<h2>JavaScript Object Constructors : Weekly Events</h2>
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
 
-<p id="demo"></p>
-
-<script>
-// Constructor function for weeklyEvent objects
-  // parameters should be taken from the form
-function weeklyEvent(eventName, eventWeekday, startTime, endTime) {
-  this.name = eventName;
-  this.weekday = eventWeekday;
-  this.start = startTime;
-  this.end = endTime;
-}
-
-// Create an object to test with
-//need to have this happen within the php file
-const CSEClass = new weeklyEvent("CSE 201", "W", "8:30", "9:50");
-const ENGClass = new weeklyEvent("ENG 313", "M", "1:15", "2:30");
-
-// Display class
-document.getElementById("demo").innerHTML =
-"I have " + CSEClass.name + " at " + CSEClass.start + " " + CSEClass.weekday + "."; 
-</script>
-
-</body>
-</html>
+con.connect(function(err) {
+  if (err) throw err;
+  con.query("INSERT INTO Calendar(name, desc, weekday, startTime, endTime) VALUES """, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
