ee0edee2c4c1d42cbb5dda7113b8f02a13bcb280
braydenm02 <99356492+braydenm02@users.noreply.github.com>
2023-11-30 18:21:01 -0500

    Update weeklyEvent.js

--- js/weeklyEvent.js
+++ js/weeklyEvent.js
@@ -1,18 +1,20 @@
 // adds an event to the database
 function addEvent(name, description, weekday, startTime, endTime) {
-var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "yourusername",
-  password: "yourpassword",
-  database: "mydb"
-});
+  var mysql = require('mysql');
 
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("INSERT INTO Calendar(name, desc, weekday, startTime, endTime) VALUES ('name', 'description', 'weekday', 'startTime', 'endTime')", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
+  var con = mysql.createConnection({
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword",
+    database: "mydb"
   });
-});
+
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query("INSERT INTO Calendar(name, desc, weekday, startTime, endTime) VALUES ('name', 'description', 'weekday', 'startTime', 'endTime')", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
+};
