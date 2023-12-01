10f2abbcb4cde6434090daa198f5d2d75f84edd0
dageeem <149324186+dageeem@users.noreply.github.com>
2023-11-30 19:05:25 -0500

    Update deleteEvent.js

--- js/deleteEvent.js
+++ js/deleteEvent.js
@@ -1,18 +1,6 @@
-var mysql = require('mysql');
+function deleteEvent(name) {
 
-var con = mysql.createConnection({
-  host: "",
-  user: "",
-  password: "",
-  database: ""
-});
-
-con.connect(function(err, id) {
-  if(err) throw err;
-
-  mysql.input('id1', mysql.VarChar, id);
-
-  con.query( "DELETE FROM ___ WHERE id = @id1", function (err, result, feilds) {
+con.query( "DROP FROM Calendar WHERE id = @id1", function (err, result, feilds) {
     if (err) throw err;
     console.log(result);
   });
