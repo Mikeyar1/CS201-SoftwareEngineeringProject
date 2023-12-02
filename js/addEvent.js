c7ef5ecb8d5485c1f5c6c732218c2bf1f55b69a7
braydenm02 <99356492+braydenm02@users.noreply.github.com>
2023-12-02 18:30:43 -0500

    Update addEvent.js

--- js/addEvent.js
+++ js/addEvent.js
@@ -1,16 +1,9 @@
 
 
 // adds an event to the database
-function addEvent(name, description, weekday, startTime, endTime) {
-  
- function addEvent(name, description, weekday, startTime, endTime) {
-    // SQL string with placeholders for values
-    let sqlstr = "INSERT INTO Calendar(name, desc, weekday, startTime, endTime) VALUES ('name', 'description', 'weekday', 'startTime', 'endTime')";
-    
-    let stmt = db.prepare(sqlstr);
+function addEvent(form) {
+  let inputValue = form.inputbox.value;
+  alert ("You typed: " + inputValue);
+}
 
-    stmt.bind([name, description, weekday, startTime, endTime]);
-
-    stmt.run();
- }
  
