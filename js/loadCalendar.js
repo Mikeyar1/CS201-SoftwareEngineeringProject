e9bc2bed9926461d09b3ef4edcab6bb0628ed046
braydenm02 <99356492+braydenm02@users.noreply.github.com>
2023-12-02 17:46:44 -0500

    Update loadCalendar.js

--- js/loadCalendar.js
+++ js/loadCalendar.js
@@ -1,6 +1,8 @@
+import initSqlJs from "sql.js";
+
 // load everything from database
 function addEvent(name, description, weekday, startTime, endTime) {
 
   
-    let sqlstr = "INSERT INTO Calendar(name, desc, weekday, startTime, endTime) VALUES ('name', 'description', 'weekday', 'startTime', 'endTime')";
-    db.run(sqlstr); // Run the query without returning anything
+  
+}
