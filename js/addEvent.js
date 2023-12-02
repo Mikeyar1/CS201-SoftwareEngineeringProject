f2a5fe63c4dac918e1c5e8a54cdda9e20c929129
Mikeyar1 <77662442+Mikeyar1@users.noreply.github.com>
2023-12-02 18:01:32 -0500

    Update addEvent.js

--- js/addEvent.js
+++ js/addEvent.js
@@ -5,7 +5,7 @@ function addEvent(name, description, weekday, startTime, endTime) {
   
  function addEvent(name, description, weekday, startTime, endTime) {
     // SQL string with placeholders for values
-    let sqlstr = "INSERT INTO Calendar(name, desc, weekday, startTime, endTime) VALUES (?, ?, ?, ?, ?)";
+    let sqlstr = "INSERT INTO Calendar(name, desc, weekday, startTime, endTime) VALUES ('name', 'description', 'weekday', 'startTime', 'endTime')";
     
     let stmt = db.prepare(sqlstr);
 
